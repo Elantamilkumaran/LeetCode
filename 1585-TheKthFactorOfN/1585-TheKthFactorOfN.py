c1994961d0f3c07c7d043686c3a7e502e7b1d2da
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-01-08 15:32:59 +0530

    Create 1585-TheKthFactorOfN

---
+++ 1585-TheKthFactorOfN/1585-TheKthFactorOfN.py
@@ -0,0 +1,15 @@
+# Last updated: 1/8/2026, 3:32:57 PM
+class Solution(object):
+    def kthFactor(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        c=0
+        for i in range(1,n+1):
+            if n%i==0:
+                c+=1
+                if c==k:
+                    return i
+        return -1
