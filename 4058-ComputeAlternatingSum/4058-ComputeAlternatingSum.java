87ff55cb93fee58506a3b08aa0867b8b1803bebd
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-01-08 15:31:14 +0530

    Create 4058-ComputeAlternatingSum

---
+++ 4058-ComputeAlternatingSum/4058-ComputeAlternatingSum.java
@@ -0,0 +1,11 @@
+// Last updated: 1/8/2026, 3:31:12 PM
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0) sum+=nums[i];
+            else sum-=nums[i];
+        }
+        return sum;
+    }
+}
