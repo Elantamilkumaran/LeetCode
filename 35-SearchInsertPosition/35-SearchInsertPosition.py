6e7fc01579610463f548e7ac2375495398855eeb
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-01-08 15:34:56 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -0,0 +1,14 @@
+# Last updated: 1/8/2026, 3:34:53 PM
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
