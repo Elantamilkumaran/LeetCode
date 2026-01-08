1982ced233ad515838b6d60f59ef5cdf17474cbf
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-01-08 15:34:08 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.java
@@ -0,0 +1,10 @@
+// Last updated: 1/8/2026, 3:34:06 PM
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans=new int[n+1];
+        for(int i=0;i<=n;i++){
+            ans[i]=Integer.bitCount(i);
+        }
+        return ans;
+    }
+}
