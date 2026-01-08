452851a340ade29f442089b689b157dc88a60cfa
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-01-08 15:33:42 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.py
@@ -0,0 +1,8 @@
+# Last updated: 1/8/2026, 3:33:40 PM
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s.lower()
