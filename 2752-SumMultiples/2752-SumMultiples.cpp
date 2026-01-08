f80b2b7b2c683b5f4b1d93734ac3ef8de1aeab48
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-01-08 15:31:53 +0530

    Create 2752-SumMultiples

---
+++ 2752-SumMultiples/2752-SumMultiples.cpp
@@ -0,0 +1,14 @@
+// Last updated: 1/8/2026, 3:31:51 PM
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int s=0;
+        for(int i=0;i<=n;i++){
+            if(i%3==0 || i%5==0 || i%7==0){
+                s+=i;
+            }
+        }
+        return s;
+        
+    }
+};
