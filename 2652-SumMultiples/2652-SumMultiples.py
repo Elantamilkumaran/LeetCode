a89de3b5aec96f36776f9f247c602c41b5a6a621
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:48:57 +0530

    Create 2652-SumMultiples

---
+++ 2652-SumMultiples/2652-SumMultiples.py
@@ -0,0 +1,12 @@
+# Last updated: 9/10/2026, 9:48:57 AM
+class Solution(object):
+    def sumOfMultiples(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        s=0
+        for i in range(1,n+1):
+            if (i%3==0 or i%5==0 or i%7==0):
+                s+=i
+        return s
