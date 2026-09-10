753bb9c5488836bb805f137ee59477ecd5eb15d2
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:51:31 +0530

    Create 2006-CountNumberOfPairsWithAbsoluteDifferenceK

---
+++ 2006-CountNumberOfPairsWithAbsoluteDifferenceK/2006-CountNumberOfPairsWithAbsoluteDifferenceK.java
@@ -0,0 +1,14 @@
+// Last updated: 9/10/2026, 9:51:31 AM
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(Math.abs(nums[i]-nums[j])==k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
