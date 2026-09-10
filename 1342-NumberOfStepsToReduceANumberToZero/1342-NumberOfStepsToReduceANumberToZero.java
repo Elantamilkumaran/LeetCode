1e469a13585be611fcdb6850219686605a0652e3
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:54:27 +0530

    Create 1342-NumberOfStepsToReduceANumberToZero

---
+++ 1342-NumberOfStepsToReduceANumberToZero/1342-NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,17 @@
+// Last updated: 9/10/2026, 9:54:27 AM
+class Solution {
+    public int numberOfSteps(int num) {
+        int c=0;
+        while(num!=0){
+            if(num%2==0){
+                num/=2;
+                c++;
+            }
+            else{
+                num-=1;
+                c++;
+            }
+        }
+        return c;
+    }
+}
