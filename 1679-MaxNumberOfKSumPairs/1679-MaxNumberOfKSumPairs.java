951e00a9dac89d77de9c7bb3ac3922630041ac22
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:52:50 +0530

    Create 1679-MaxNumberOfKSumPairs

---
+++ 1679-MaxNumberOfKSumPairs/1679-MaxNumberOfKSumPairs.java
@@ -0,0 +1,23 @@
+// Last updated: 9/10/2026, 9:52:50 AM
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        int left=0;
+        int right=nums.length-1;
+        int count=0;
+        Arrays.sort(nums);
+        while(left<right){
+            if((nums[left]+nums[right])==k){
+                count++;
+                left++;
+                right--;
+            }
+            else if((nums[left]+nums[right])<k){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return count;
+    }
+}
