a55f46356a1155ccb3a5ffd149eca3f8c501d7b4
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:49:32 +0530

    Create 2481-MinimumCutsToDivideACircle

---
+++ 2481-MinimumCutsToDivideACircle/2481-MinimumCutsToDivideACircle.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/10/2026, 9:49:32 AM
+class Solution {
+public:
+    int numberOfCuts(int n) {
+        if(n==1){
+            return 0;
+        }
+        else if(n%2==0){
+            return n/2;
+        }
+        else{
+            return n;
+        }
+    }
+};
