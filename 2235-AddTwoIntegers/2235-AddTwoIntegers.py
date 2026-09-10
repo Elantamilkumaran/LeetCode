29e08a3e64ed31899d6bfb111008d1b92be71235
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:50:27 +0530

    Create 2235-AddTwoIntegers

---
+++ 2235-AddTwoIntegers/2235-AddTwoIntegers.py
@@ -0,0 +1,10 @@
+# Last updated: 9/10/2026, 9:50:27 AM
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1+num2
+        
