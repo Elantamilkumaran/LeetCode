f929483d173ee7531c9901acdf748cd7ea070398
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:55:01 +0530

    Create 1190-ReverseSubstringsBetweenEachPairOfParentheses

---
+++ 1190-ReverseSubstringsBetweenEachPairOfParentheses/1190-ReverseSubstringsBetweenEachPairOfParentheses.java
@@ -0,0 +1,30 @@
+// Last updated: 9/10/2026, 9:55:01 AM
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                stack.push(ch);
+                continue;
+            }
+            else if(ch == ')'){
+                StringBuilder str = new StringBuilder();
+                while(stack.peek() != '('){
+                    str.append(stack.pop());
+                }
+                stack.pop();
+                for(char i : str.toString().toCharArray()){
+                    stack.push(i);
+                }
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        while(!stack.isEmpty()){
+            ans.append(stack.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
