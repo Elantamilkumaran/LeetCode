5fe8c1f012279d79bc84a69f667097993926b3e7
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:51:48 +0530

    Create 1929-ConcatenationOfArray

---
+++ 1929-ConcatenationOfArray/1929-ConcatenationOfArray.py
@@ -0,0 +1,9 @@
+# Last updated: 9/10/2026, 9:51:48 AM
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums*2
+        
