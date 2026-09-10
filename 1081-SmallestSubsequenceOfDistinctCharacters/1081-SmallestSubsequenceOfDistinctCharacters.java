89fcd1c6b2ef42f9cee278162f579011596bdb40
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:55:28 +0530

    Create 1081-SmallestSubsequenceOfDistinctCharacters

---
+++ 1081-SmallestSubsequenceOfDistinctCharacters/1081-SmallestSubsequenceOfDistinctCharacters.java
@@ -0,0 +1,27 @@
+// Last updated: 9/10/2026, 9:55:27 AM
+class Solution {
+    public String smallestSubsequence(String s) {
+        int[] lastIndex=new int[26];
+        for(int i=0;i<s.length();i++){
+            lastIndex[s.charAt(i)-'a']=i;
+        }
+        Stack<Character> st=new Stack<>();
+        int[] lastSeen=new int[26];
+        for(int i=0;i<s.length();i++){
+            if(lastSeen[s.charAt(i)-'a']==1){
+                continue;
+            }
+            while(!st.isEmpty() && i<lastIndex[st.peek()-'a'] && st.peek()>s.charAt(i)){
+                char c = st.pop();
+                lastSeen[c-'a']=0;
+            }
+            st.push(s.charAt(i));
+            lastSeen[s.charAt(i)-'a']=1;
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
