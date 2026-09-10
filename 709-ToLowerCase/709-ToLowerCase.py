05c66096870c4d10330e3141a12b30e529577c40
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:57:05 +0530

    Create 709-ToLowerCase

---
+++ 709-ToLowerCase/709-ToLowerCase.py
@@ -0,0 +1,8 @@
+# Last updated: 9/10/2026, 9:57:04 AM
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s.lower()
