0aed77743b2f7354c9fd0c9713a0a57c2176ba27
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:46:56 +0530

    Create 3131-FindTheIntegerAddedToArrayI

---
+++ 3131-FindTheIntegerAddedToArrayI/3131-FindTheIntegerAddedToArrayI.java
@@ -0,0 +1,12 @@
+// Last updated: 9/10/2026, 9:46:55 AM
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        int sum1=0;
+        int sum2=0;
+        for(int i=0;i<nums1.length;i++){
+            sum1+=nums1[i];
+            sum2+=nums2[i];
+        }
+        return (sum2-sum1)/nums2.length;
+    }
+}
