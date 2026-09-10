d4019902e471ddff9d98385d42e9d5f58d14ac5d
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:53:03 +0530

    Create 1672-RichestCustomerWealth

---
+++ 1672-RichestCustomerWealth/1672-RichestCustomerWealth.py
@@ -0,0 +1,14 @@
+# Last updated: 9/10/2026, 9:53:03 AM
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        m=0
+        for i in accounts:
+            s=sum(i)
+            if s>m :
+                m=s
+        return m
+        
