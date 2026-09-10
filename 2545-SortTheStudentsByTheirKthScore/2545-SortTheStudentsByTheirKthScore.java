8c260306d5f80f7181cb37299bf7938acba7243e
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:49:06 +0530

    Create 2545-SortTheStudentsByTheirKthScore

---
+++ 2545-SortTheStudentsByTheirKthScore/2545-SortTheStudentsByTheirKthScore.java
@@ -0,0 +1,18 @@
+// Last updated: 9/10/2026, 9:49:05 AM
+class Solution {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        //Arrays.sort(score,(x,y)->Integer.compare(y[k],x[k]));
+        //return score;
+        int n=score.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(score[i][k]<score[j][k]){
+                    int[] temp=score[i];
+                    score[i]=score[j];
+                    score[j]=temp;
+                }
+            }
+        }
+        return score;
+    }
+}
