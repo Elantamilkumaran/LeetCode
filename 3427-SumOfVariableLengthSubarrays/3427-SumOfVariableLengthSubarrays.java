8368806375b5ab6e0d1ed7e1cdba700131d950dd
Elantamilkumaran <elantamilkumaran@gmail.com>
2026-09-10 09:46:06 +0530

    Create 3427-SumOfVariableLengthSubarrays

---
+++ 3427-SumOfVariableLengthSubarrays/3427-SumOfVariableLengthSubarrays.java
@@ -0,0 +1,15 @@
+// Last updated: 9/10/2026, 9:46:05 AM
+class Solution {
+    public int subarraySum(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            int start=Math.max(0,i-nums[i]);
+            int sum=0;
+            for(int j=start;j<=i;j++){
+                sum+=nums[j];
+            }
+            ans+=sum;
+        }
+        return ans;
+    }
+}
